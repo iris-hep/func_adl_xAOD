eef4a333f15cc92d0f4c7cc6b9b2b357bb4c8f24
Baidyanath Kundu <kundubaidya99@gmail.com>
2021-04-07 12:37:13 +0530

    Add support for file list in cms template

--- func_adl_xAOD/template/cms/r5/analyzer_cfg.py
+++ func_adl_xAOD/template/cms/r5/analyzer_cfg.py
@@ -6,10 +6,16 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(-1))
 
+fileNamesArray = []
+with open('/scripts/filelist.txt', 'r') as flist:
+    for line in flist.readlines():
+        fileNamesArray.append("file:" + line)
+
+fileNamesTuple = tuple(fileNamesArray)
 process.source = cms.Source("PoolSource",
                             # replace 'myfile.root' with the source file you want to use
                             fileNames=cms.untracked.vstring(
-                                'root://eospublic.cern.ch//eos/opendata/cms/Run2012C/DoubleMuParked/AOD/22Jan2013-v1/10000/F2878994-766C-E211-8693-E0CB4EA0A939.root'
+                                *fileNamesTuple
                             )
                             )
 
