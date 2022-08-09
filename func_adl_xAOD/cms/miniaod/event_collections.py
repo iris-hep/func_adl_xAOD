04baa480c01abb7710258f441df189a786cca006
Haoran Sun <hsun3@uw.edu>
2022-08-08 23:53:07 -0700

    First commit. Built a basic skeleton for miniaod backend. Tests will not pass as it is still in construction

---
+++ func_adl_xAOD/cms/miniaod/event_collections.py
@@ -0,0 +1,67 @@
+from typing import Union
+import func_adl_xAOD.common.cpp_types as ctyp
+from func_adl_xAOD.common.event_collections import (
+    EventCollectionSpecification, event_collection_coder, event_collection_collection_container, event_collection_container)
+
+
+# There is no use for a CMS single item collection - everything they have
+# has multiple items in it. Copy from the ATLAS example to get this working correctly.
+
+
+class cms_miniaod_event_collection_collection(event_collection_collection_container):
+    def __init__(self, type_name: Union[str, ctyp.CPPParsedTypeInfo],
+                 element_name: Union[str, ctyp.CPPParsedTypeInfo],
+                 p_depth_type: int = 1, p_depth_element: int = 0):
+        super().__init__(type_name, element_name, p_depth_element=p_depth_element, p_depth_type=p_depth_type)
+
+    def __str__(self):
+        return f"edm::Handle<{self.type}>"
+
+
+# all the collections types that are available. This is required because C++
+# is strongly typed, and thus we have to transmit this information.
+cms_miniaod_collections = [
+    EventCollectionSpecification('cms', "Muons",
+                                 ['DataFormats/PatCandidates/interface/Muon.h'],
+                                 cms_miniaod_event_collection_collection('pat::MuonCollection', 'pat::Muon'),
+                                 [],
+                                 ),
+    EventCollectionSpecification('cms', "Vertex",
+                                 ["DataFormats/VertexReco/interface/Vertex.h",
+                                  "DataFormats/VertexReco/interface/VertexFwd.h"
+                                  ],
+                                 cms_miniaod_event_collection_collection('reco::VertexCollection', 'reco::Vertex', p_depth_element=0),
+                                 [],
+                                 ),
+    EventCollectionSpecification('cms', "Electrons",
+                                 ['DataFormats/PatCandidates/interface/Electron.h',
+                                  'DataFormats/EgammaCandidates/interface/GsfElectron.h'
+                                  ],
+                                 cms_miniaod_event_collection_collection('pat::ElectronCollection', 'pat::Electron'),
+                                 [],
+                                 ),
+]
+
+
+def define_default_cms_types():
+    'Define the default cms types'
+    ctyp.add_method_type_info("reco::Muon", "globalTrack", ctyp.terminal('reco::Track', p_depth=1))
+    ctyp.add_method_type_info("reco::Muon", "hitPattern", ctyp.terminal('reco::HitPattern'))
+    ctyp.add_method_type_info("reco::Muon", "isPFIsolationValid", ctyp.terminal('bool'))
+    ctyp.add_method_type_info("reco::Muon", "isPFMuon", ctyp.terminal('bool'))
+    ctyp.add_method_type_info("reco::Muon", "pfIsolationR04", ctyp.terminal('reco::MuonPFIsolation'))
+
+    ctyp.add_method_type_info("reco::GsfElectron", "gsfTrack", ctyp.terminal('reco::GsfTrack', p_depth=1))
+    ctyp.add_method_type_info("reco::GsfElectron", "isEB", ctyp.terminal('bool'))
+    ctyp.add_method_type_info("reco::GsfElectron", "isEE", ctyp.terminal('bool'))
+    ctyp.add_method_type_info("reco::GsfElectron", "passingPflowPreselection", ctyp.terminal('bool'))
+    ctyp.add_method_type_info("reco::GsfElectron", "superCluster", ctyp.terminal('reco::SuperClusterRef', p_depth=1))
+    ctyp.add_method_type_info("reco::GsfElectron", "pfIsolationVariables", ctyp.terminal('reco::GsfElectron::PflowIsolationVariables'))
+
+    ctyp.add_method_type_info("reco::GsfTrack", "trackerExpectedHitsInner", ctyp.terminal('reco::HitPattern'))  # reco::HitPattern is the expected return type
+
+
+class cms_event_collection_coder(event_collection_coder):
+    def get_running_code(self, container_type: event_collection_container) -> list:
+        return [f'{container_type} result;',
+                'iEvent.getByToken(collection_name, result);']
--- tests/cms/aod/test_local_dataset.py
+++ tests/cms/aod/test_local_dataset.py
@@ -1,4 +1,4 @@
-from .config import f_location
+from tests.cms.aod.config import f_location
 import pytest
 
 python_on_whales = pytest.importorskip("python_on_whales")
