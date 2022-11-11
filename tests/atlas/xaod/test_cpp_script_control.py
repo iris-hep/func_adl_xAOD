4cc06ea6e44ac5676e3a562b677b4b88e17776c3
Gordon Watts <gwatts@uw.edu>
2022-11-10 23:46:54 -0800

    Run on modern test container
    * Use most recently released
    * Update to use proper sys errors
    * Black formatting

--- tests/atlas/xaod/test_cpp_script_control.py
+++ tests/atlas/xaod/test_cpp_script_control.py
@@ -16,7 +16,7 @@ from .config import local_path, run_long_running_tests
 
 pytestmark = run_long_running_tests
 
-ExecutorInfo = namedtuple('ExecutorInfo', 'main_script output_filename')
+ExecutorInfo = namedtuple("ExecutorInfo", "main_script output_filename")
 
 
 class hash_event_dataset(EventDataset):
@@ -34,29 +34,33 @@ class hash_event_dataset(EventDataset):
 
 @pytest.fixture()
 def cache_directory():
-    'Return a directory that can be deleted when the test is done'
+    "Return a directory that can be deleted when the test is done"
     with tempfile.TemporaryDirectory() as d_temp:
         yield Path(d_temp)
 
 
 def generate_test_jet_fetch(cache_dir: Path):
-    '''
+    """
     Generate an expression and C++ files, etc., that contains code for a valid C++ run
-    '''
-    return hash_event_dataset(cache_dir) \
-        .SelectMany('lambda e: e.Jets("AntiKt4EMTopoJets")') \
-        .Select('lambda j: j.pt()/1000.0') \
+    """
+    return (
+        hash_event_dataset(cache_dir)
+        .SelectMany('lambda e: e.Jets("AntiKt4EMTopoJets")')
+        .Select("lambda j: j.pt()/1000.0")
         .value()
+    )
 
 
 def generate_test_jet_fetch_bad(cache_dir: Path):
-    '''
+    """
     Generate an expression and C++ files, etc., that contains code for a valid C++ run
-    '''
-    return hash_event_dataset(cache_dir) \
-        .SelectMany('lambda e: e.Jets("AntiKt4EMTopoJets")') \
-        .Select('lambda j: j.ptt()/1000.0') \
+    """
+    return (
+        hash_event_dataset(cache_dir)
+        .SelectMany('lambda e: e.Jets("AntiKt4EMTopoJets")')
+        .Select("lambda j: j.ptt()/1000.0")
         .value()
+    )
 
 
 class docker_run_error(Exception):
@@ -70,7 +74,7 @@ class docker_runner:
         self.results_dir = results_dir
 
     def compile(self, info):
-        'Run the script as a compile'
+        "Run the script as a compile"
 
         results_dir = tempfile.TemporaryDirectory()
         docker_cmd = f'docker exec {self._name} /bin/bash -c "cd /home/atlas; /scripts/{info.main_script} -c"'
@@ -80,18 +84,18 @@ class docker_runner:
         return results_dir
 
     def run(self, info: ExecutorInfo, files: List[Path]):
-        'Run the docker command'
+        "Run the docker command"
 
         # Unravel the file path. How we do this depends on how we are doing this work.
         filename = files[0].name
 
         # Write the file list into a filelist in the scripts directory. If that isn't going to be what we do, then
         # create it as a cmd line option.
-        cmd_options = ''
-        cmd_options += f'-d /data/{filename} '
+        cmd_options = ""
+        cmd_options += f"-d /data/{filename} "
 
         # We are just going to run
-        cmd_options += '-r '
+        cmd_options += "-r "
 
         # Docker command
         results_dir = tempfile.TemporaryDirectory()
@@ -103,40 +107,46 @@ class docker_runner:
 
 
 class docker_running_container:
-    '''
+    """
     This will start up a docker container running our analysis base.
-    '''
+    """
 
     def __init__(self, info, code_dir: str, files: List[Path]):
-        'Init with directories for mapping, etc'
+        "Init with directories for mapping, etc"
 
         self._code_dir = code_dir
         self._files = files
 
     def __enter__(self):
-        'Get the docker command up and running'
+        "Get the docker command up and running"
         data_dir = self._files[0].parent
         self._results_dir = tempfile.TemporaryDirectory()
-        docker_cmd = f'docker run --name test_func_xAOD --rm -d -v {self._code_dir}:/scripts:ro -v {str(self._results_dir.name)}:/results -v {data_dir.absolute()}:/data:ro atlas/analysisbase:latest /bin/bash -c "while [ 1 ] ; do sleep 1; echo hi ; done"'
+        docker_cmd = f'docker run --name test_func_xAOD --rm -d -v {self._code_dir}:/scripts:ro -v {str(self._results_dir.name)}:/results -v {data_dir.absolute()}:/data:ro atlas/analysisbase:21.2.234 /bin/bash -c "while [ 1 ] ; do sleep 1; echo hi ; done"'
         r = os.system(docker_cmd)
         if r != 0:
-            raise Exception(f'Unable to start docker deamon: {r}')
-        return docker_runner('test_func_xAOD', self._results_dir.name)
+            raise Exception(f"Unable to start docker deamon: {r}")
+        return docker_runner("test_func_xAOD", self._results_dir.name)
 
     def __exit__(self, type, value, traceback):
         with self._results_dir:
-            r = os.system('docker rm -f test_func_xAOD')
+            r = os.system("docker rm -f test_func_xAOD")
             if r != 0:
-                raise Exception(f'Unable to stop docker container: {r}')
+                raise Exception(f"Unable to stop docker container: {r}")
 
 
-def run_docker(info: ExecutorInfo, code_dir: str, files: List[str],
-               data_file_on_cmd_line: bool = False,
-               compile_only: bool = False, run_only: bool = False,
-               add_position_argument_at_start: Optional[str] = None,
-               extra_flag: Optional[str] = None,
-               output_dir: Optional[str] = None, mount_output: bool = True) -> TemporaryDirectory:
-    'Run the docker command'
+def run_docker(
+    info: ExecutorInfo,
+    code_dir: str,
+    files: List[str],
+    data_file_on_cmd_line: bool = False,
+    compile_only: bool = False,
+    run_only: bool = False,
+    add_position_argument_at_start: Optional[str] = None,
+    extra_flag: Optional[str] = None,
+    output_dir: Optional[str] = None,
+    mount_output: bool = True,
+) -> TemporaryDirectory:
+    "Run the docker command"
 
     # Unravel the file path. How we do this depends on how we are doing this work.
     assert len(files) == 1
@@ -146,38 +156,40 @@ def run_docker(info: ExecutorInfo, code_dir: str, files: List[str],
 
     # Write the file list into a filelist in the scripts directory. If that isn't going to be what we do, then
     # create it as a cmd line option.
-    cmd_options = ''
+    cmd_options = ""
     if data_file_on_cmd_line:
-        cmd_options += f'-d /data/{filename} '
+        cmd_options += f"-d /data/{filename} "
     else:
-        with open(os.path.join(code_dir, 'filelist.txt'), 'w') as f_out:
-            f_out.writelines([f'/data/{filename}'])
+        with open(os.path.join(code_dir, "filelist.txt"), "w") as f_out:
+            f_out.writelines([f"/data/{filename}"])
 
     # Compile or run only?
     if compile_only:
-        cmd_options += '-c '
+        cmd_options += "-c "
     if run_only:
-        cmd_options += '-r '
+        cmd_options += "-r "
 
     # Extra random flag
     if extra_flag is not None:
-        cmd_options += f'{extra_flag} '
+        cmd_options += f"{extra_flag} "
 
     if output_dir is not None:
-        cmd_options += f'-o {output_dir} '
+        cmd_options += f"-o {output_dir} "
     else:
-        output_dir = '/results'
+        output_dir = "/results"
 
     results_dir = tempfile.TemporaryDirectory()
-    mount_output_options = f'-v {str(results_dir.name)}:{output_dir}' if mount_output else ''
+    mount_output_options = (
+        f"-v {str(results_dir.name)}:{output_dir}" if mount_output else ""
+    )
 
     # Add an argument at the start?
-    initial_args = ''
+    initial_args = ""
     if add_position_argument_at_start is not None:
-        initial_args = f'{add_position_argument_at_start} '
+        initial_args = f"{add_position_argument_at_start} "
 
     # Docker command
-    docker_cmd = f'docker run --rm -v {code_dir}:/scripts:ro {mount_output_options} -v {base_dir.absolute()}:/data:ro atlas/analysisbase:latest /scripts/{info.main_script} {initial_args} {cmd_options}'
+    docker_cmd = f"docker run --rm -v {code_dir}:/scripts:ro {mount_output_options} -v {base_dir.absolute()}:/data:ro atlas/analysisbase:21.2.234 /scripts/{info.main_script} {initial_args} {cmd_options}"
     result = os.system(docker_cmd)
     if result != 0:
         raise docker_run_error(f"nope, that didn't work {result}!")
@@ -185,7 +197,7 @@ def run_docker(info: ExecutorInfo, code_dir: str, files: List[str],
 
 
 def test_good_cpp_total_run(cache_directory):
-    'Good C++, and no arguments that does full run'
+    "Good C++, and no arguments that does full run"
 
     info = generate_test_jet_fetch(cache_directory)
     with run_docker(info, cache_directory, [local_path]) as result_dir:
@@ -193,51 +205,65 @@ def test_good_cpp_total_run(cache_directory):
 
 
 def test_good_cpp_total_run_output_dir(cache_directory):
-    'Good C++, and no arguments that does full run'
+    "Good C++, and no arguments that does full run"
 
     info = generate_test_jet_fetch(cache_directory)
-    with run_docker(info, cache_directory, [local_path], output_dir='/home/atlas/results') as result_dir:
+    with run_docker(
+        info, cache_directory, [local_path], output_dir="/home/atlas/results"
+    ) as result_dir:
         assert os.path.exists(os.path.join(result_dir, info.output_filename))
 
 
 def test_good_cpp_total_run_output_dir_no_mount(cache_directory):
-    'Good C++, and no arguments that does full run'
+    "Good C++, and no arguments that does full run"
 
     info = generate_test_jet_fetch(cache_directory)
-    with run_docker(info, cache_directory, [local_path], output_dir='/home/atlas/results', mount_output=False):
+    with run_docker(
+        info,
+        cache_directory,
+        [local_path],
+        output_dir="/home/atlas/results",
+        mount_output=False,
+    ):
         # We aren't mounting so we can't look. So we just want to make sure no errors occur.
         pass
 
 
 def test_good_cpp_total_run_file_as_arg(cache_directory):
-    'Bad C++ generated, should throw an exception'
+    "Bad C++ generated, should throw an exception"
 
     info = generate_test_jet_fetch(cache_directory)
-    with run_docker(info, cache_directory, [local_path], data_file_on_cmd_line=True) as result_dir:
+    with run_docker(
+        info, cache_directory, [local_path], data_file_on_cmd_line=True
+    ) as result_dir:
         assert os.path.exists(os.path.join(result_dir, info.output_filename))
 
 
 def test_bad_cpp_total_run(cache_directory):
-    'Bad C++, and no arguments that does full run'
+    "Bad C++, and no arguments that does full run"
 
     try:
         info = generate_test_jet_fetch_bad(cache_directory)
-        with run_docker(info, cache_directory, [local_path], data_file_on_cmd_line=True):
+        with run_docker(
+            info, cache_directory, [local_path], data_file_on_cmd_line=True
+        ):
             assert False
     except docker_run_error:
         pass
 
 
 def test_good_cpp_just_compile(cache_directory):
-    'Good C++, only do the compile'
+    "Good C++, only do the compile"
 
     info = generate_test_jet_fetch(cache_directory)
-    with run_docker(info, cache_directory, [local_path], compile_only=True) as result_dir:
+    with run_docker(
+        info, cache_directory, [local_path], compile_only=True
+    ) as result_dir:
         assert not os.path.exists(os.path.join(result_dir, info.output_filename))
 
 
 def test_bad_cpp_just_compile(cache_directory):
-    'Bad C++, only do the compile'
+    "Bad C++, only do the compile"
 
     try:
         info = generate_test_jet_fetch_bad(cache_directory)
@@ -248,7 +274,7 @@ def test_bad_cpp_just_compile(cache_directory):
 
 
 def test_good_cpp_compile_and_run(cache_directory):
-    'Good C++, first do the compile, and then do the run'
+    "Good C++, first do the compile, and then do the run"
 
     info = generate_test_jet_fetch(cache_directory)
     with docker_running_container(info, cache_directory, [Path(local_path)]) as runner:
@@ -258,7 +284,7 @@ def test_good_cpp_compile_and_run(cache_directory):
 
 
 def test_good_cpp_compile_and_run_2_files(cache_directory):
-    'Make sure we can run a second file w/out seeing errors'
+    "Make sure we can run a second file w/out seeing errors"
     info = generate_test_jet_fetch(cache_directory)
     with docker_running_container(info, cache_directory, [Path(local_path)]) as runner:
         runner.compile(info)
@@ -270,17 +296,22 @@ def test_good_cpp_compile_and_run_2_files(cache_directory):
 
 
 def test_run_with_bad_position_arg(cache_directory):
-    'Pass in a bogus argument at the end with no flag'
+    "Pass in a bogus argument at the end with no flag"
     try:
         info = generate_test_jet_fetch(cache_directory)
-        with run_docker(info, cache_directory, [local_path], add_position_argument_at_start="/results"):
+        with run_docker(
+            info,
+            cache_directory,
+            [local_path],
+            add_position_argument_at_start="/results",
+        ):
             assert False
     except docker_run_error:
         pass
 
 
 def test_run_with_bad_flag(cache_directory):
-    'Pass in a bogus flag'
+    "Pass in a bogus flag"
     try:
         info = generate_test_jet_fetch(cache_directory)
         with run_docker(info, cache_directory, [local_path], extra_flag="-k"):
--- tests/atlas/xaod/utils.py
+++ tests/atlas/xaod/utils.py
@@ -7,10 +7,8 @@ import pandas as pd
 import uproot
 from func_adl.object_stream import ObjectStream
 from func_adl_xAOD.atlas.xaod.executor import atlas_xaod_executor
-from func_adl_xAOD.atlas.xaod.query_ast_visitor import \
-    atlas_xaod_query_ast_visitor
-from func_adl_xAOD.common.cpp_representation import (cpp_sequence,
-                                                     cpp_variable, set_rep)
+from func_adl_xAOD.atlas.xaod.query_ast_visitor import atlas_xaod_query_ast_visitor
+from func_adl_xAOD.common.cpp_representation import cpp_sequence, cpp_variable, set_rep
 from func_adl_xAOD.common.util_scope import top_level_scope
 from tests.utils.base import LocalFile, dataset, dummy_executor  # type: ignore
 
@@ -41,7 +39,7 @@ class AtlasXAODDockerException(Exception):
 
 class AtlasXAODLocalFile(LocalFile):
     def __init__(self, local_files: Union[Path, List[Path]]):
-        super().__init__("atlas/analysisbase:latest", "query.cxx", local_files)
+        super().__init__("atlas/analysisbase:21.2.234", "query.cxx", local_files)
 
     def raise_docker_exception(self, message: str):
         raise AtlasXAODDockerException(message)
@@ -51,13 +49,15 @@ class AtlasXAODLocalFile(LocalFile):
 
 
 async def exe_from_qastle(q: str):
-    'Dummy executor that will return the ast properly rendered. If qastle_roundtrip is true, then we will round trip the ast via qastle first.'
+    "Dummy executor that will return the ast properly rendered. If qastle_roundtrip is true, then we will round trip the ast via qastle first."
     # Round trip qastle if requested.
     import qastle
+
     a = qastle.text_ast_to_python_ast(q).body[0].value
 
     # Setup the rep for this filter
     from func_adl import find_EventDataset
+
     file = find_EventDataset(a)
     iterator = cpp_variable("bogus-do-not-use", top_level_scope(), cpp_type=None)
     set_rep(file, cpp_sequence(iterator, iterator, top_level_scope()))
@@ -69,7 +69,7 @@ async def exe_from_qastle(q: str):
 
 
 def load_root_as_pandas(file: Path) -> pd.DataFrame:
-    '''Given the result from a query as a ROOT file path, return
+    """Given the result from a query as a ROOT file path, return
     the contents as a pandas dataframe.
 
     Args:
@@ -77,16 +77,16 @@ def load_root_as_pandas(file: Path) -> pd.DataFrame:
 
     Returns:
         pandas.DataFrame: [description]
-    '''
+    """
     assert isinstance(file, Path)
     assert file.exists()
 
     with uproot.open(file) as input:
-        return input['atlas_xaod_tree'].arrays(library='pd')  # type: ignore
+        return input["atlas_xaod_tree"].arrays(library="pd")  # type: ignore
 
 
 def load_root_as_awkward(file: Path) -> ak.Array:
-    '''Given the result from a query as a ROOT file path, return
+    """Given the result from a query as a ROOT file path, return
     the contents as a pandas dataframe.
 
     Args:
@@ -94,52 +94,54 @@ def load_root_as_awkward(file: Path) -> ak.Array:
 
     Returns:
         pandas.DataFrame: [description]
-    '''
+    """
     assert isinstance(file, Path)
     assert file.exists()
 
     with uproot.open(file) as input:
-        return input['atlas_xaod_tree'].arrays()  # type: ignore
+        return input["atlas_xaod_tree"].arrays()  # type: ignore
 
 
 def as_pandas(o: ObjectStream) -> pd.DataFrame:
-    '''Return a query as a pandas dataframe.
+    """Return a query as a pandas dataframe.
 
     Args:
         o (ObjectStream): The query
 
     Returns:
         pd.DataFrame: The result
-    '''
+    """
     return load_root_as_pandas(o.value())
 
 
 def as_awkward(o: ObjectStream) -> ak.Array:
-    '''Return a query as a pandas dataframe.
+    """Return a query as a pandas dataframe.
 
     Args:
         o (ObjectStream): The query
 
     Returns:
         pd.DataFrame: The result
-    '''
+    """
     return load_root_as_awkward(o.value())
 
 
 async def as_pandas_async(o: ObjectStream) -> pd.DataFrame:
-    '''Return a query as a pandas dataframe.
+    """Return a query as a pandas dataframe.
 
     Args:
         o (ObjectStream): The query
 
     Returns:
         pd.DataFrame: The result
-    '''
+    """
     return load_root_as_pandas(await o.value_async())
 
 
-def ast_parse_with_replacement(ast_string: str, replacements: Dict[str, ast.AST]) -> ast.AST:
-    '''Returns an ast, where any ast.Name elements are replaced by the ast that is in
+def ast_parse_with_replacement(
+    ast_string: str, replacements: Dict[str, ast.AST]
+) -> ast.AST:
+    """Returns an ast, where any ast.Name elements are replaced by the ast that is in
     the dict.
 
     Args:
@@ -148,7 +150,7 @@ def ast_parse_with_replacement(ast_string: str, replacements: Dict[str, ast.AST]
 
     Returns:
         (ast.AST): Ast from the parse, with the names replaced.
-    '''
+    """
     a = ast.parse(ast_string)
 
     class replacer(ast.NodeTransformer):
