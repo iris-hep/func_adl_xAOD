4d6e516ee8f51fcf57554e44796db95880d64c36
Gordon Watts <gwatts@uw.edu>
2024-03-02 16:08:19 -0800

    Remove bogus import

--- func_adl_xAOD/common/cpp_types.py
+++ func_adl_xAOD/common/cpp_types.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import copy
 from dataclasses import dataclass
-from typing import Dict, List, Optional, Union
+from typing import Dict, Optional, Union
 
 
 @dataclass
