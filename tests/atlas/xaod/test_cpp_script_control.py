52caf2d5e41eee4a2a85d23f28b711f6c2f480e5
Gordon Watts <gwatts@uw.edu>
2025-04-11 10:39:46 -0700

    Reset permissions

--- tests/atlas/xaod/test_cpp_script_control.py
+++ tests/atlas/xaod/test_cpp_script_control.py
@@ -189,6 +189,7 @@ def run_docker(
         initial_args = f"{add_position_argument_at_start} "
 
     # Docker command
+    os.chmod(os.path.join(code_dir, info.main_script), 0o755)
     docker_cmd = f"docker run --rm -v {code_dir}:/scripts:ro {mount_output_options} -v {base_dir.absolute()}:/data:ro gitlab-registry.cern.ch/atlas/athena/analysisbase:25.2.42 /scripts/{info.main_script} {initial_args} {cmd_options}"
     result = os.system(docker_cmd)
     if result != 0:
