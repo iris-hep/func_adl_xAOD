5bc3e826055a6301a3912e881d7c3c0325e14444
Gordon Watts <gwatts@uw.edu>
2025-04-11 11:28:30 -0700

    Yet another attempt

--- tests/atlas/xaod/test_cpp_script_control.py
+++ tests/atlas/xaod/test_cpp_script_control.py
@@ -189,7 +189,7 @@ def run_docker(
         initial_args = f"{add_position_argument_at_start} "
 
     # Docker command
-    docker_cmd = f'docker run --rm -v {code_dir}:/scripts:rw {mount_output_options} -v {base_dir.absolute()}:/data:ro gitlab-registry.cern.ch/atlas/athena/analysisbase:25.2.42 bash -c "ls -l; ls -l /scripts; chmod a+x /scripts/{info.main_script}; /scripts/{info.main_script} {initial_args} {cmd_options}"'
+    docker_cmd = f'docker run --rm -v {code_dir}:/scripts:rw {mount_output_options} -v {base_dir.absolute()}:/data:ro gitlab-registry.cern.ch/atlas/athena/analysisbase:25.2.42 bash -c "ls -l; ls -l /scripts; source /scripts/{info.main_script} {initial_args} {cmd_options}"'
     result = os.system(docker_cmd)
     if result != 0:
         raise docker_run_error(f"nope, that didn't work {result}!")
