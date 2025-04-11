be63828bf75d150265f64a27e041c8ea35491823
Gordon Watts <gwatts@uw.edu>
2025-04-11 16:53:36 -0700

    Add some security stuff

--- tests/atlas/xaod/test_cpp_script_control.py
+++ tests/atlas/xaod/test_cpp_script_control.py
@@ -190,7 +190,7 @@ def run_docker(
 
     # Docker command
     assert Path(code_dir).exists()
-    docker_cmd = f'docker run --rm -v {code_dir}:/scripts:rw {mount_output_options} -v {base_dir.absolute()}:/data:ro gitlab-registry.cern.ch/atlas/athena/analysisbase:25.2.42 bash -c "ls -l /; ls -l /scripts; source /scripts/{info.main_script} {initial_args} {cmd_options}"'
+    docker_cmd = f'docker run --rm -v {code_dir}:/scripts:rw,z {mount_output_options} -v {base_dir.absolute()}:/data:ro gitlab-registry.cern.ch/atlas/athena/analysisbase:25.2.42 bash -c "chmod -R a+rw /scripts; ls -l /; ls -l /scripts; source /scripts/{info.main_script} {initial_args} {cmd_options}"'
     result = os.system(docker_cmd)
     if result != 0:
         raise docker_run_error(f"nope, that didn't work {result}!")
