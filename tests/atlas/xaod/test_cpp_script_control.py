ba9e4f40fbe6e424b07325b303536c74d82df954
Gordon Watts <gwatts@uw.edu>
2025-04-11 21:45:23 -0700

    Fix up permissions

--- tests/atlas/xaod/test_cpp_script_control.py
+++ tests/atlas/xaod/test_cpp_script_control.py
@@ -185,7 +185,7 @@ def run_docker(
         mount_output_options = (
             f"-v {str(results_path)}:{output_dir}" if mount_output else ""
         )
-        results_path.chmod(Path(results_path).stat().st_mode | 0o500)
+        results_path.chmod(Path(results_path).stat().st_mode | 0o555)
 
     # Add an argument at the start?
     initial_args = ""
@@ -194,7 +194,7 @@ def run_docker(
 
     # Docker command
     os.system("ls -l /tmp")
-    code_dir.chmod(code_dir.stat().st_mode | 0o500)
+    code_dir.chmod(code_dir.stat().st_mode | 0o555)
     os.system("echo after chmod")
     os.system("ls -l /tmp")
     docker_cmd = f'docker run --rm {mount_output_options} -v {base_dir.absolute()}:/data:ro -v {code_dir.absolute()}:/scripts:ro gitlab-registry.cern.ch/atlas/athena/analysisbase:25.2.42 bash -c "ls -l /;ls -l /home/atlas;/scripts/{info.main_script} {initial_args} {cmd_options}"'
